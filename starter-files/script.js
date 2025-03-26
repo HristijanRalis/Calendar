c9d1d0ca54b4202e4d9ef7fd4c15ab2b82456800
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-03-26 15:49:44 +0100

    Refactoring the code

--- starter-files/script.js
+++ starter-files/script.js
@@ -6,16 +6,18 @@ const monthNameCurrent = document.getElementById("month-name");
 const yearCurrent = document.getElementById("year");
 
 const today = new Date();
-const dayNum = today.getDate();
-const day = today.toLocaleDateString("un-US", { weekday: "long" });
-const monthName = today.toLocaleDateString("en-US", { month: "long" });
-const year = today.getFullYear();
-// function getDayName(today, locale) {
-//   let dateDay = new Date(today);
-//   return dateDay.toLocaleDateString(locale, { weekday: "long" });
-// }
+// const dayNum = today.getDate();
+// const day = today.toLocaleDateString("un-US", { weekday: "long" });
+// const monthName = today.toLocaleDateString("en-US", { month: "long" });
+// const year = today.getFullYear();
+const dateOptions = {
+  dayNum: today.getDate(),
+  day: today.toLocaleDateString("en-US", { weekday: "long" }),
+  monthName: today.toLocaleDateString("en-US", { month: "long" }),
+  year: today.getFullYear(),
+};
 
-dayNumberCurrent.textContent = dayNum;
-dayNameCurrent.textContent = day;
-monthNameCurrent.textContent = monthName;
-yearCurrent.textContent = year;
+dayNumberCurrent.textContent = dateOptions.dayNum;
+dayNameCurrent.textContent = dateOptions.day;
+monthNameCurrent.textContent = dateOptions.monthName;
+yearCurrent.textContent = dateOptions.year;
