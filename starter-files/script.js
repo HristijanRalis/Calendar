7163cae3ab5dd6d0422b9a7ceb4aaddce5059bea
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-03-26 14:49:57 +0100

    Added some feature on script

--- starter-files/script.js
+++ starter-files/script.js
@@ -7,14 +7,13 @@ const yearCurrent = document.getElementById("year");
 
 const today = new Date();
 const dayNum = today.getDate();
+const day = today.toLocaleDateString("un-US", { weekday: "long" });
 const monthName = today.toLocaleDateString("en-US", { month: "long" });
 const year = today.getFullYear();
-function getDayName(today, locale) {
-  let dateDay = new Date(today);
-  return dateDay.toLocaleDateString(locale, { weekday: "long" });
-}
-
-const day = getDayName(today, "en-US");
+// function getDayName(today, locale) {
+//   let dateDay = new Date(today);
+//   return dateDay.toLocaleDateString(locale, { weekday: "long" });
+// }
 
 dayNumberCurrent.textContent = dayNum;
 dayNameCurrent.textContent = day;
