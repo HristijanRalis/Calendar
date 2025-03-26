09a352f272c7ca58378529722fecf4e93258e6c5
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-03-26 15:51:49 +0100

    Make more cleaner and reusable

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,23 +1,29 @@
 // CALENDAR
 
-const dayNumberCurrent = document.getElementById("day-number");
-const dayNameCurrent = document.getElementById("day-name");
-const monthNameCurrent = document.getElementById("month-name");
-const yearCurrent = document.getElementById("year");
-
-const today = new Date();
 // const dayNum = today.getDate();
 // const day = today.toLocaleDateString("un-US", { weekday: "long" });
 // const monthName = today.toLocaleDateString("en-US", { month: "long" });
 // const year = today.getFullYear();
-const dateOptions = {
-  dayNum: today.getDate(),
-  day: today.toLocaleDateString("en-US", { weekday: "long" }),
-  monthName: today.toLocaleDateString("en-US", { month: "long" }),
-  year: today.getFullYear(),
+
+const updateCalendar = function () {
+  const dayNumberCurrent = document.getElementById("day-number");
+  const dayNameCurrent = document.getElementById("day-name");
+  const monthNameCurrent = document.getElementById("month-name");
+  const yearCurrent = document.getElementById("year");
+
+  const today = new Date();
+
+  const dateOptions = {
+    dayNum: today.getDate(),
+    day: today.toLocaleDateString("en-US", { weekday: "long" }),
+    monthName: today.toLocaleDateString("en-US", { month: "long" }),
+    year: today.getFullYear(),
+  };
+
+  dayNumberCurrent.textContent = dateOptions.dayNum;
+  dayNameCurrent.textContent = dateOptions.day;
+  monthNameCurrent.textContent = dateOptions.monthName;
+  yearCurrent.textContent = dateOptions.year;
 };
 
-dayNumberCurrent.textContent = dateOptions.dayNum;
-dayNameCurrent.textContent = dateOptions.day;
-monthNameCurrent.textContent = dateOptions.monthName;
-yearCurrent.textContent = dateOptions.year;
+updateCalendar();
